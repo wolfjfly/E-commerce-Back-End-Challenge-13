a4bf3a67f8eab5a5fb855869ac2c5482ffee9979
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-25 09:44:06 -0600

    commit

--- models/index.js
+++ models/index.js
@@ -14,12 +14,12 @@ Category.hasMany(Product,{
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany( Tag,{
-  through: 'ProductTag_id',
+  foreignKey: 'ProductTag_id',
 
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany( Product,{
-  through: 'ProductTag_id',
+  foreignKey: 'ProductTag_id',
 });
 
 module.exports = {
